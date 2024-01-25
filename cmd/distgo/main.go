18ed30dd67c0a16a001ba4083cc91b77e5dbb333
badbubble <liuhanjun369@gmail.com>
2024-01-25 19:54:30 +0100

    reformat code && add zap log

--- cmd/distgo/main.go
+++ cmd/distgo/main.go
@@ -1,76 +1,64 @@
 package main
 
 import (
+	"distgo/internal/dao/mq"
+	"distgo/internal/dao/redis"
+	"distgo/internal/logger"
 	"distgo/internal/setting"
-	"distgo/pkg/mq"
 	"distgo/pkg/parser"
-	"distgo/pkg/redis"
-	"fmt"
 	"github.com/hibiken/asynq"
+	"go.uber.org/zap"
 	"log"
-	"os"
-	"os/exec"
 	"time"
 )
 
 const ConfigFilePath = "configs/distgo.yaml"
-
-func getGoBuildCommands(projectPath string) string {
-	command := fmt.Sprintf("cd %s && go build -x -a -work main.go", projectPath)
-	cmd := exec.Command("sh", "-c", command)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("generate command error: %v", err)
-	}
-	file, err := os.Create("commands.sh")
-	if err != nil {
-		fmt.Println("Error creating the file:", err)
-
-	}
-	defer file.Close()
-
-	// Write the string to the file
-	_, err = file.WriteString(string(output))
-	if err != nil {
-		fmt.Println("Error writing to the file:", err)
-	}
-	return string(output)
-}
+const ProjectPath = "/Users/badbubble/GolandProjects/BubblePL"
+const MainFile = "main.go"
 
 func main() {
-	err := setting.Init("/home/badbubble/distgo/configs/distgo.yaml")
-	if err != nil {
-		log.Fatalf("read settings failed: %v", err)
+	// read configurations
+	if err := setting.Init(ConfigFilePath); err != nil {
+		log.Fatalf("read configurations failed: %v", err)
 	}
-	err = redis.Init(setting.Conf.RedisConfig)
-	if err != nil {
-		log.Fatalf("create redis client failed: %v", err)
+	// initiate logger
+	if err := logger.Init(setting.Conf.LogConfig, setting.Conf.Mode); err != nil {
+		log.Fatalf("initiate logger failed: %v", err)
+	}
+	// connect to redis
+	if err := redis.Init(setting.Conf.RedisConfig); err != nil {
+		log.Fatalf("connect to client failed: %v", err)
+	}
+	// create asynq
+	if err := mq.InitClient(setting.Conf.AsynqConfig); err != nil {
+		log.Fatalf("create asynq client failed: %v", err)
 	}
 
-	client := asynq.NewClient(asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%d", setting.Conf.AsynqConfig.Host, setting.Conf.RedisConfig.Port)})
-
-	// generate the go build commands
-	projectPath := "/home/badbubble/BubblePL"
-	commands := getGoBuildCommands("/home/badbubble/BubblePL")
 	//split the go build commands to compileJobs
-	compileJobs, err := parser.New(commands, projectPath)
+	compileJobs, err := parser.New(ProjectPath, MainFile)
 	if err != nil {
-		log.Fatalf("create compile jobs error: %v", compileJobs)
+		zap.L().Error("parser.New failed",
+			zap.String("ProjectPath", ProjectPath),
+			zap.String("MainFile", MainFile),
+			zap.Error(err),
+		)
+		return
 	}
-	// send the compileJobs to asynq
-
+	// send the compileJobs to Asynq
 	for _, job := range compileJobs {
 		task, err := mq.NewCompileJob(job)
 		if err != nil {
-			log.Fatalf("send to asynq failed: %v", err)
+			zap.L().Error("failed to create new job ",
+				zap.Any("Job", job),
+				zap.Error(err),
+			)
+			return
 		}
-		_, err = client.Enqueue(task, asynq.Retention(24*time.Hour), asynq.MaxRetry(10), asynq.Timeout(3*time.Minute))
-		if err != nil {
-			log.Fatalf("could not enqueue task: %v", err)
+		if _, err = mq.AsynqClient.Enqueue(task, asynq.Retention(24*time.Hour), asynq.MaxRetry(10)); err != nil {
+			zap.L().Error("failed to put task in asynq",
+				zap.Any("task", task),
+				zap.Error(err),
+			)
 		}
 	}
-
-	// collect the complieJobs Result from asynq
-	// finish the compiling
-
 }
