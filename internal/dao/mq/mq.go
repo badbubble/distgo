b351a0d69461570ab6a144d9083664fbc2b0d62f
badbubble <liuhanjun369@gmail.com>
2024-02-08 23:44:31 +0100

    create Compile_Job & Compile_Group queue

--- internal/dao/mq/mq.go
+++ internal/dao/mq/mq.go
@@ -9,17 +9,33 @@ import (
 var AsynqClient *asynq.Client
 var AsynqServer *asynq.Server
 
-func InitServer(config *setting.AsynqConfig) error {
+func InitServerWorker(config *setting.AsynqConfig) error {
 	AsynqServer = asynq.NewServer(
 		asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%d", config.Host, config.Port)},
 		asynq.Config{
 			// Specify how many concurrent workers to use
 			Concurrency: config.Concurrency,
+			Queues: map[string]int{
+				"Compile_Job": 1,
+			},
 		},
 	)
 	return nil
 }
 
+func InitServerCoordinator(config *setting.AsynqConfig) error {
+	AsynqServer = asynq.NewServer(
+		asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%d", config.Host, config.Port)},
+		asynq.Config{
+			// Specify how many concurrent workers to use
+			Concurrency: config.Concurrency,
+			Queues: map[string]int{
+				"Compile_Group": 1,
+			},
+		},
+	)
+	return nil
+}
 func InitClient(config *setting.AsynqConfig) error {
 	AsynqClient = asynq.NewClient(asynq.RedisClientOpt{Addr: fmt.Sprintf("%s:%d", config.Host, config.Port)})
 	return nil
